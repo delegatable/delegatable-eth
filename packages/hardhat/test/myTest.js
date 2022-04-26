d4621ce8faeaa3522f7079cc058d9e116cb2e06c
Dan Finlay <dan@danfinlay.com>
2022-04-25 19:01:35 -0700

    Transitive delegation test passed

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -16,7 +16,8 @@ const { TIMEOUT } = require("dns");
 const types = signTypedDataify(friendlyTypes);
 const CONTRACT_NAME = 'YourContract';
 const ownerHexPrivateKey = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-const delegateHexPrivKey = '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
+const account1PrivKey = '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
+const account2PrivKey = '5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a';
 
 describe(CONTRACT_NAME, function () {
 
@@ -56,8 +57,7 @@ describe(CONTRACT_NAME, function () {
       authority: '0x0000000000000000000000000000000000000000000000000000000000000000',
       caveats: [],
     };
-    const primaryType = 'Delegation';
-    const typedMessage = createTypedMessage(yourContract, delegation, primaryType);
+    const typedMessage = createTypedMessage(yourContract, delegation, 'Delegation');
 
     // Owner signs the delegation:
     const privateKey = fromHexString(ownerHexPrivateKey);
@@ -69,9 +69,10 @@ describe(CONTRACT_NAME, function () {
       signature,
       delegation,
     }
+
     // Delegate signs the invocation message:
     const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
-    const delegatePrivateKey = fromHexString(delegateHexPrivKey);
+    const delegatePrivateKey = fromHexString(account1PrivKey);
     const invocationMessage = {
       batch: [{
         authority: [signedDelegation],
@@ -98,6 +99,92 @@ describe(CONTRACT_NAME, function () {
 
     // A third party can submit the invocation method to the chain:
     const res = await yourContract.connect(addr2).invoke([signedInvocation]);
+
+    // Verify the change was made:
+    expect(await yourContract.purpose()).to.equal(targetString);
+  })
+
+  it('delegates can delegate', async () => {
+    const [_owner, addr1, addr2, addr3] = await ethers.getSigners();
+    console.log(`owner: ${_owner.address}`);
+    console.log(`addr1: ${addr1.address}`);
+    console.log(`addr2: ${addr2.address}`);
+    console.log(`addr3: ${addr3.address}`);
+
+    const targetString = 'A totally DELEGATED purpose!'
+    const yourContract = await deployContract();
+
+    // Prepare the delegation message:
+    // This message has no caveats, and authority 0,
+    // so it is a simple delegation to addr1 with no restrictions,
+    // and will allow the delegate to perform any action the signer could perform on this contract.
+    const delegation = {
+      delegate: addr1.address,
+      authority: '0x0000000000000000000000000000000000000000000000000000000000000000',
+      caveats: [],
+    };
+    const primaryType = 'Delegation';
+    const typedMessage = createTypedMessage(yourContract, delegation, primaryType);
+
+    // Owner signs the delegation:
+    const privateKey = fromHexString(ownerHexPrivateKey);
+    const signature = sigUtil.signTypedData_v4(
+      privateKey,
+      typedMessage
+    );
+    const signedDelegation = {
+      signature,
+      delegation,
+    }
+
+    // First delegate signs the second delegation:
+    const delegationHash = TypedDataUtils.hashStruct('SignedDelegation', signedDelegation, types, true);
+    const delegation2 = {
+      delegate: addr2.address,
+      authority: delegationHash,
+      caveats: [],
+    };
+    const typedMessage2 = createTypedMessage(yourContract, delegation2, 'Delegation');
+
+    const privateKey2 = fromHexString(account1PrivKey);
+    const signature2 = sigUtil.signTypedData_v4(
+      privateKey2,
+      typedMessage2
+    );
+    const signedDelegation2 = {
+      signature: signature2,
+      delegation: delegation2,
+    }
+
+    // Second delegate signs the invocation message:
+    const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
+    const delegatePrivateKey = fromHexString(account2PrivKey);
+    const invocationMessage = {
+      batch: [{
+        authority: [signedDelegation, signedDelegation2],
+        replayProtection: {
+          nonce: '0x0000000000000000000000000000000000000000000000000000000000000000',
+          queue: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        },
+        transaction: {
+          to: yourContract.address,
+          gasLimit: '10000000000000000',
+          data: desiredTx.data,
+        },
+      }],
+    };
+    const typedInvocationMessage = createTypedMessage(yourContract, invocationMessage, 'Invocations');
+    const invocationSig = sigUtil.signTypedData_v4(
+      delegatePrivateKey,
+      typedInvocationMessage
+    );
+    const signedInvocation = {
+      signature: invocationSig,
+      invocations: invocationMessage,
+    }
+
+    // A third party can submit the invocation method to the chain:
+    const res = await yourContract.connect(addr3).invoke([signedInvocation]);
 
     // Verify the change was made:
     expect(await yourContract.purpose()).to.equal(targetString);
